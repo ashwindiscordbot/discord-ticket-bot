207c50470ccaae1f62bf92b9438f5faaf429b540
ashwindiscordbot <famapp22899914@gmail.com>
2026-03-12 23:14:47 +0530

    Update ticketbot.py

--- ticketbot.py
+++ ticketbot.py
@@ -1,45 +1,47 @@
 # ticketbot.py
 import discord
 from discord.ext import commands
+from datetime import timedelta
 import os
 
-# Read token from environment variable (Replit secret)
+# Get your token from Replit Secrets
 TOKEN = os.environ['TOKEN']
 
-# Enable all intents
+# Enable all intents (make sure Privileged Intents are ON in Developer Portal)
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Store ticket channels in memory
-ticket_channels = {}
-
+# When bot is ready
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('Bot is online and ready!')
 
 # Command to create a ticket
 @bot.command()
-async def ticket(ctx, *, reason=None):
+async def ticket(ctx):
+    """Create a private ticket channel."""
     guild = ctx.guild
     overwrites = {
         guild.default_role: discord.PermissionOverwrite(read_messages=False),
         ctx.author: discord.PermissionOverwrite(read_messages=True)
     }
-    channel_name = f"ticket-{ctx.author.name}"
-    ticket_channel = await guild.create_text_channel(channel_name, overwrites=overwrites, reason=reason)
-    ticket_channels[ctx.author.id] = ticket_channel.id
-    await ticket_channel.send(f"{ctx.author.mention}, your ticket has been created! Reason: {reason if reason else 'No reason provided'}")
-    await ctx.send(f"{ctx.author.mention}, your ticket channel is {ticket_channel.mention}")
+    ticket_channel = await guild.create_text_channel(
+        name=f'ticket-{ctx.author.name}', overwrites=overwrites
+    )
+    await ticket_channel.send(f"Hello {ctx.author.mention}, support will be with you soon!")
+    await ctx.send(f"Ticket created: {ticket_channel.mention}", delete_after=5)
 
 # Command to close a ticket
 @bot.command()
 async def close(ctx):
-    if ctx.channel.id in ticket_channels.values():
+    """Close the ticket channel."""
+    if ctx.channel.name.startswith("ticket-"):
         await ctx.send("Closing this ticket in 5 seconds...")
-        await discord.utils.sleep_until(discord.utils.utcnow() + discord.timedelta(seconds=5))
+        await asyncio.sleep(5)  # Wait 5 seconds before deletion
         await ctx.channel.delete()
     else:
-        await ctx.send("This command can only be used inside a ticket channel.")
+        await ctx.send("This command can only be used in ticket channels.", delete_after=5)
 
+# Run the bot
 bot.run(TOKEN)
